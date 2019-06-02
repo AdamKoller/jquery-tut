3af59943a530cd730d090d93533b20d48312f598
AdamKoller <adamkoller.86@gmail.com>
2019-06-01 22:45:58 -0500

    'third'

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-$('body').css({background: 'pink'})
+$(function() {
+    $(window).resize(function() {
+        if ($(window).width() > 1000) {
+            $('body').css({
+                background: 'purple'
+            })
+        } else if ($(window).width() > 700) {
+            $('body').css({
+                background:'gold'
+            }) 
+        } else {
+            $('body').css({
+                background:'pink'
+            })
+        }
+    })
+});
