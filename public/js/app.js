b7042bd14430434f2aef3d61fc2bd9c1389f9af4
AdamKoller <adamkoller.86@gmail.com>
2019-06-05 20:46:40 -0500

    comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,15 @@
 $(function() {
     $(window).resize(function() {
-        if ($(window).width() > 1000) {
+        if ($(window).width() > 1000) { // if window is greater than 1000px, change color to purple
             $('body').css({
                 background: 'purple'
             })
-        } else if ($(window).width() > 700) {
+        } else if ($(window).width() > 700) { // if window greater than 700 but less than 1000, change to gold
             $('body').css({
                 background:'gold'
             }) 
         } else {
-            $('body').css({
+            $('body').css({ // if window less than 700, change to pink
                 background:'pink'
             })
         }
